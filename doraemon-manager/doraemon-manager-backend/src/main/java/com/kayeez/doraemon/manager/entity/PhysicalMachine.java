a6860a7cf8c0aaa147a5647c30f94a03f3148e08
zhao kai <zhaokaikaikover@gmail.com>
2020-06-08 12:39:55 +0800

    update git ignore file

---
+++ doraemon-manager/doraemon-manager-backend/src/main/java/com/kayeez/doraemon/manager/entity/PhysicalMachine.java
@@ -0,0 +1,8 @@
+package com.kayeez.doraemon.manager.entity;
+
+public class PhysicalMachine {
+    private String ip;
+    private Integer sshPort;
+    private String sshUsername;
+    private String sshPassword;
+}
